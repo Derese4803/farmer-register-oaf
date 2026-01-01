40f9a655617096b242c976e0bde18c804c52bffe
Derese4803 <derese.esubalew-c@oneacrefund.org>
2026-01-01 15:21:38 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,19 +5,6 @@ import datetime
 
 Base = declarative_base()
 
-class Woreda(Base):
-    __tablename__ = 'woredas'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True, nullable=False)
-    kebeles = relationship("Kebele", back_populates="woreda", cascade="all, delete-orphan")
-
-class Kebele(Base):
-    __tablename__ = 'kebeles'
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    woreda_id = Column(Integer, ForeignKey('woredas.id'))
-    woreda = relationship("Woreda", back_populates="kebeles")
-
 class Farmer(Base):
     __tablename__ = 'farmers'
     id = Column(Integer, primary_key=True)
@@ -27,8 +14,8 @@ class Farmer(Base):
     kebele = Column(String)
     phone = Column(String)
     audio_data = Column(Text) 
-    registered_by = Column(String) # Stores the name of the person who did the entry
+    registered_by = Column(String) 
 
 def create_tables():
     from database import engine
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.all(bind=engine)
