831ae2313e41c1ae55c99311682b43f96133175f
Derese4803 <derese.esubalew-c@oneacrefund.org>
2026-01-01 14:37:23 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import StaticPool
 
-# Using v2 to ensure fresh schema for 2025 survey
 DATABASE_URL = "sqlite:///./amhara_survey_2025.db"
 
-engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False},
-    poolclass=StaticPool
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
